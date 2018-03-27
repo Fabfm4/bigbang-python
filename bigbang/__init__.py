0e82eba7d2a88811f32fa73accca5e73658cf227
Fabfm4 <favian.flone92@gmail.com>
2018-03-27 16:38:55 -0600

    init

--- bigbang/__init__.py
+++ bigbang/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.1.3"
+__version__ = "0.1.5"
+
+
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 from bigbang import __version__ as version
+
+
 setup(
     name='bigbang',
-    packages=find_packages(),
     version=version,
     description='Library to create development environment',
     author='Sergio Fabian Flores Mendoza',
     author_email='favian.flone92@gmail.com',
     url='https://github.com/Fabfm4/bigbang-python',
-    download_url='https://github.com/Fabfm4/bigbang-python/archive/{0}.tar.gz'.format(version),
-    keywords=['python', 'django', 'docker'],
-    scripts=['bigbang/bin/bigbang-py'],
-    entry_points={
-        'console_scripts': [
-            'bigbang-py=bigbang.commands:main',
-        ]
-    },
-    classifiers=[],
+    license='MIT',
+    packages=["bigbang"],
+    zip_safe=False,
 )
